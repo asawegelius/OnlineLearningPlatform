59e4b739f9a9beb11b18b491d2292b613f830a24
asawegelius <asa.wegelius@gmail.com>
2016-05-30 14:09:51 +0200

    small change in PID, objective prototype instead of complete system

--- Code/OLPStudentClient/src/main/java/se/wegelius/olp/servlet/CourseServlet.java
+++ Code/OLPStudentClient/src/main/java/se/wegelius/olp/servlet/CourseServlet.java
@@ -6,7 +6,6 @@
 package se.wegelius.olp.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
